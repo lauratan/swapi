1031f17c8efce9e9568ae741f6d46d38defa0839
Laura Tan <laura.tan108@gmail.com>
2020-01-21 23:27:39 -0800

    Set up required routes with corresponding API endpoints

--- swapi-server/server.js
+++ swapi-server/server.js
@@ -1,12 +1,42 @@
+const axios = require('axios');
 const express = require('express');
-// const bodyParser = require('body-parser')
-// const path = require('path');
 const app = express();
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const PORT = 8080;
+
+app.get('/ping', function(req, res) {
+  return res.send('pong');
 });
 
-app.listen(8080, () => {
-  console.log(`Star Wars App Server listening on port 8080!`);
+// Call the https://swapi.co/api/films API to get all films
+app.get('/films', (req, res) => {
+  axios
+    .get('https://swapi.co/api/films')
+    .then(({ data: { results } }) => {
+      res.json(results);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ status: 500, message: 'Error calling Star Wars API' });
+    });
+});
+
+// Call the https://swapi.co/api/films/:id API to get film details
+app.get('/film/:id', (req, res) => {
+  axios
+    .get(`https://swapi.co/api/films/${req.params.id}`)
+    .then(({ data }) => {
+      res.json(data);
+    })
+    .catch(({ response: { status, statusText } }) => {
+      res.status(status).json({
+        status,
+        message: statusText
+      });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Star Wars App Server listening on port ${PORT}!`);
 });
